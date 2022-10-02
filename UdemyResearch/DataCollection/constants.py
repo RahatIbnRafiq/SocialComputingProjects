f274b3e23e919c862040d74031034cd6d62b6db6
Rahat Ibn Rafiq <rahatibnrafiq@gmail.com>
2022-10-02 17:07:44 -0400

    Update constants.py

--- UdemyResearch/DataCollection/constants.py
+++ UdemyResearch/DataCollection/constants.py
@@ -1,5 +1,5 @@
-UDEMY_CLIENT_ID = "gBvBsv3yfCyBVcqs2BzkWewyRHwhQceIE3Q51cL0"
-UDEMY_CLIENT_SECRET = "L5lDPQuomOwd7AZTpWatfwouHOnhDduDS46RPKQbvleL50Q2xFldgLJ1A8Drs7hMqHWmKusH7s2Wdg5X46ReoGBNdBCt2gCMVC7eGdiWvEgGp06IU9N4cU7YfHuFFPlB"
+UDEMY_CLIENT_ID = ""
+UDEMY_CLIENT_SECRET = ""
 
 MONGODB_NAME = "udemy"
 CHROME_DRIVER_PATH = "/Users/rahatibnrafiq/chromedrivers_selenium/chromedriver"
